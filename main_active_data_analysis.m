79faabbfe98c30927d237e903308ad1c8c6fba7b
Binh Nguyen <binh.nguyen@ryerson.ca>
2021-11-17 15:18:49 -0500

    Add files via upload

--- main_active_data_analysis.m
+++ main_active_data_analysis.m
@@ -11,21 +11,26 @@ format long;
 
 %% Import Data
 path = '/Users/binhnguyen/Downloads/Participant Data/';
+subject =  15;
+
 
 subjects = cell (1,8);
 subjects{1} = 'U0099725385';
 subjects{2} = 'U0571781825';
 subjects{3} = 'U1320514293';
 subjects{4} = 'U1431916954';
-subjects{5} = 'U3354287398';
-subjects{6} = 'U6404631153';
-subjects{7} = 'U7529931256';
-subjects{8} = 'U7835074301';
-subjects{8} = 'U7835074301';
-subjects{9} = 'U8509049816';
-subjects{10}= 'U8920175053';
+subjects{5} = 'U2104195387';
+subjects{6} = 'U3029850491';
+subjects{7} = 'U3354287398';
+subjects{8} = 'U5666408227';
+subjects{9} = 'U6404631153';
+subjects{10} = 'U7291247359';
+subjects{11} = 'U7529931256';
+subjects{12} = 'U7835074301';
+subjects{13} = 'U8509049816';
+subjects{14}= 'U8920175053';
+subjects{15} = 'U9647150890';
 
-subject = 1;
 
 filename = strcat (path, subjects{subject},'/result_event.json');
 
@@ -79,13 +84,44 @@ for i = 2:length (q_indicies)
 end
 
 % Summing
-Legend = [Legend; "Total"]
-Questionnaire_Answered(end +1) = sum(Questionnaire_Answered(1:end))
+Legend = [Legend; "Total"];
+Questionnaire_Answered(end+1) = sum(Questionnaire_Answered(1:end));
 
-table_analysis = table(Legend,transpose (Questionnaire_Answered));
+try 
+    % Date time
+    Time_Date = [(datetime(active_data(1).timestamp,'ConvertFrom','epochtime','TicksPerSecond',1000))];
+    for i = 2:length (q_indicies)
+       Time_Date = [Time_Date ; (datetime(active_data(i).timestamp,'ConvertFrom','epochtime','TicksPerSecond',1000))];
+    end
+
+    Time_Date = [Time_Date; (datetime(0,'ConvertFrom','epochtime','TicksPerSecond',1000))];
+    
+catch 
+    Time_Date = [];
+    for i = 1:length (q_indicies)+1
+       Time_Date (i,1) =  0;
+    end
+end
+
+% Table
+
+table_analysis = table(Legend, transpose(Questionnaire_Answered),'VariableNames',{'Legend' 'Questionnaire'});
 
 
 %% Display everything
 
 fprintf('Subject %s \n', subjects{subject})
+
 table_analysis
+
+
+%% Beginning and End of timestamps
+
+t1 = (datetime(active_data(1).timestamp,'ConvertFrom','epochtime','TicksPerSecond',1000));
+t2 = (datetime(active_data(end).timestamp,'ConvertFrom','epochtime','TicksPerSecond',1000));
+
+fprintf('Start \n');
+t1
+fprintf('End \n');
+t2
+
